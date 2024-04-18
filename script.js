cf0537e6ba139b64f8fa9b982d3718a81f680dae
JoãoV3c0r <jvsantosc02@gmail.com>
2024-04-18 14:00:02 -0300

    Commit -Projeto Random

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+const button = document.querySelector("button")
+
+function generateButton(){
+    const min = document.querySelector(".input-min").value
+    const max = document.querySelector(".input-max").value
+
+    console.log(min, max)
+}
+
+button.addEventListener("click", generateButton)
+
+*/
+
+function generateButton(){
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+    const text = document.querySelector("p")
+
+    
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    text.innerHTML = result
+}
